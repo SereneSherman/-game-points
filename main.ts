cca77bafed803f7cbab1075f833c046bd7697e67
SereneSherman <122543877+SereneSherman@users.noreply.github.com>
2023-01-31 13:10:37 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,13 +8,49 @@ function Update_Scores () {
     OLED.newLine()
     OLED.writeStringNewLine("Ties" + ties)
 }
-let rounds = 0
-let ties = 0
-let Player_1 = 0
-OLED.init(128, 64)
-OLED.writeStringNewLine("Wanna play a game?")
-Player_1 = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # . # .
+        `)
+    Player_1 += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . # # . .
+        . # . # .
+        . # # . .
+        . # . # .
+        . # # . .
+        `)
+})
+input.onGesture(Gesture.Shake, function () {
+    reset()
+})
+function reset () {
+    OLED.init(128, 64)
+    OLED.writeStringNewLine("Wanna play a game?")
+    Player_1 = 0
+    player_2 = 0
+    ties = 0
+    rounds = 0
+    basic.pause(1000)
+    Update_Scores()
+}
 let player_2 = 0
-ties = 0
-rounds = 0
-basic.pause(1000)
+let ties = 0
+let rounds = 0
+let Player_1 = 0
+reset()
